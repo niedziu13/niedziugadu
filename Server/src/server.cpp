bb2f6d190b1a7eecb66c1614dcfc30c542d3d31b
niedziu <dawid.niedzwiecki@gmail.com>
2018-07-20 11:24:06 +0200

    CMake template

---
+++ Server/src/server.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Siema tutaj serwer" << std::endl;
+	return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Siema" << std::endl;
+	return 0;
+}
